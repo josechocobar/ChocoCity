8e1a93f939ee0104ffff7ff328a66dda618f7e75
gatodelapolice <36702908+gatodelapolice@users.noreply.github.com>
2020-05-04 14:09:01 -0300

    Proyecto Entero

---
+++ app/src/main/java/com/cuty/chococity/Login.kt
@@ -0,0 +1,16 @@
+package com.cuty.chococity
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
+
+
